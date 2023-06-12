67ff403e7695c1d1b203ab6ed7a80f0ece3d3612
Joseph Hilby <joseph.hilby@gmail.com>
2023-06-12 12:13:13 -0500

    WIP puzzle:board.java

---
+++ mod_1_puzzle/src/main/java/Board.java
@@ -0,0 +1,163 @@
+package main.java;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Stack;
+
+public class Board {
+  private int[][] tiles;
+  private int N;
+
+  // create a board from an n-by-n array of tiles,
+  // where tiles[row][col] = tile at (row, col)
+  public Board(int[][] tiles) {
+    if (tiles == null) {
+      throw new IllegalArgumentException("tiles cannot be null");
+    } else if (tiles.length != tiles[0].length) {
+      throw new IllegalArgumentException("number of rows and columns must be equal");
+    } else if (tiles.length < 2 || tiles.length >= 128) {
+      throw new IllegalArgumentException("number of rows and columns must be between 2 and 127");
+    } else {
+      N = tiles.length;
+      this.tiles = tiles;
+    }
+  }
+
+  // string representation of this board
+  public String toString() {
+    String result = "";
+    result += N + "\n";
+    for (int row = 0; row < N; row++) {
+      for (int col = 0; col < N; col++) {
+        result += " " + tiles[row][col];
+      }
+      result += "\n";
+    }
+    return result;
+  }
+
+  // board dimension n
+  public int dimension() {
+    return N;
+  }
+
+  private boolean check(int row, int col) {
+    int tile = tiles[row][col];
+    return tile != 0 && tile != (row * N) + col + 1;
+  }
+
+  // number of tiles out of place
+  public int hamming() {
+    int hammer = 0;
+    for (int row = 0; row < N; row++) {
+      for (int col = 0; col < N; col++) {
+        if (check(row, col)) {
+          hammer++;
+        }
+      }
+    }
+    return hammer;
+  }
+
+  private int manhattanValue(int row, int col) {
+    int tile = --tiles[row][col];
+    int rowOffset = tile / N - row;
+    int colOffset = tile % N - col;
+    return Math.abs(rowOffset) + Math.abs(colOffset);
+  }
+
+  // sum of Manhattan distances between tiles and goal
+  public int manhattan() {
+    int manhattan = 0;
+    for (int row = 0; row < N; row++) {
+      for (int col = 0; col < N; col++) {
+        if (check(row, col)) {
+          manhattan += manhattanValue(row, col);
+        }
+      }
+    }
+    return manhattan;
+  }
+
+  // is this board the goal board?
+  public boolean isGoal() {
+    return false;
+  }
+
+  // does this board equal y?
+  public boolean equals(Object y) {
+    Board that;
+    if (y.getClass() != this.getClass() || y == null) {
+      return false;
+    } else {
+      that = (Board) y;
+    }
+    return (this.N == that.N && Arrays.deepEquals(this.tiles, that.tiles));
+  }
+
+  private int[] findZero() {
+    int[] zero = new int[2];
+    for (int row = 0; row < N; row++) {
+      for (int col = 0; col < N; col++) {
+        if (tiles[row][col] == 0) {
+          zero[0] = row;
+          zero[1] = col;
+          break;
+        }
+      }
+    }
+    return zero;
+  }
+
+  private int[][] copyTiles(int[][] tiles, int N) {
+    int[][] tilesCopy = new int[N][N];
+    for (int row = 0; row < N; row++) {
+      for (int col = 0; col < N; col++) {
+        tilesCopy[row][col] = tiles[row][col];
+      }
+    }
+    return tilesCopy;
+  }
+
+  private Board swap(int[] tileOne, int[] tileTwo) {
+    int [][] tilesCopy = copyTiles(tiles, N);
+    Board copy = new Board(tilesCopy);
+    int temp = copy.tiles[tileOne[0]][tileOne[1]];
+    copy.tiles[tileOne[0]][tileOne[1]] = copy.tiles[tileTwo[0]][tileTwo[1]];
+    copy.tiles[tileTwo[0]][tileTwo[1]] = temp;
+    return copy;
+  }
+
+  // all neighboring boards
+  public Iterable<Board> neighbors() {
+    Stack<Board> neighbors = new Stack<Board>();
+    int[] zero = findZero();
+    int row = zero[0];
+    int col = zero[1];
+    if (row > 0) {
+      int[] tile = {row - 1, col};
+      neighbors.push(swap(tile, zero));
+    }
+    if (row < N - 1) {
+      int[] tile = {row + 1, col};
+      neighbors.push(swap(tile, zero));
+    }
+    if (col > 0) {
+      int[] tile = {row, col - 1};
+      neighbors.push(swap(tile, zero));
+    }
+    if (col < N - 1) {
+      int[] tile = {row, col + 1};
+      neighbors.push(swap(tile, zero));
+    }
+    return neighbors;
+  }
+
+  // a board that is obtained by exchanging any pair of tiles
+  public Board twin() {
+    return null;
+  }
+
+  // unit testing (not graded)
+  public static void main(String[] args) {
+  }
+}
---
+++ mod_1_puzzle/src/test/java/BoardTest.java
@@ -0,0 +1,109 @@
+package test.java;
+
+import org.junit.Test;
+
+import main.java.Board;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.core.IsEqual;
+import org.hamcrest.core.IsInstanceOf;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class BoardTest {
+  @Test
+  public void createBoard() {
+    Board board = new Board(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 0}});
+    MatcherAssert.assertThat(board, IsInstanceOf.instanceOf(Board.class));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void createBoardWithNullTiles() {
+    new Board(null);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void createBoardWithDifferentNumberOfRowsAndColumns() {
+    new Board(new int[][]{{1, 2, 3}, {4, 5, 6}});
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void createBoardWithLessThanTwoRowsAndColumns() {
+    new Board(new int[][]{{1}, {2}});
+  }
+
+  @Test
+  public void toStringTest() {
+    Board board = new Board(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 0}});
+    MatcherAssert.assertThat(board.toString(), IsEqual.equalTo("3\n 1 2 3\n 4 5 6\n 7 8 0\n"));
+  }
+
+  @Test
+  public void dimensionTest() {
+    Board board = new Board(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 0}});
+    MatcherAssert.assertThat(board.dimension(), IsEqual.equalTo(3));
+  }
+
+  @Test
+  public void hammerTest() {
+    Board board = new Board(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 0}});
+    MatcherAssert.assertThat(board.hamming(), IsEqual.equalTo(0));
+
+    Board badBoardOne = new Board(new int[][]{{1, 2, 3}, {4, 0, 6}, {7, 8, 5}});
+    MatcherAssert.assertThat(badBoardOne.hamming(), IsEqual.equalTo(1));
+
+    Board badBoardTwo = new Board(new int[][] {{8, 1, 3}, {4, 0, 2}, {7, 6, 5}});
+    MatcherAssert.assertThat(badBoardTwo.hamming(), IsEqual.equalTo(5));
+  }
+
+  @Test
+  public void manhattanTest() {
+    Board board = new Board(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 0}});
+    MatcherAssert.assertThat(board.manhattan(), IsEqual.equalTo(0));
+
+    Board badBoardOne = new Board(new int[][]{{1, 2, 3}, {4, 0, 6}, {7, 8, 5}});
+    MatcherAssert.assertThat(badBoardOne.manhattan(), IsEqual.equalTo(2));
+
+    Board badBoardTwo = new Board(new int[][]{{8, 1, 3}, {4, 0, 2}, {7, 6, 5}});
+    MatcherAssert.assertThat(badBoardTwo.manhattan(), IsEqual.equalTo(10));
+  }
+
+  @Test
+  public void equalsTest() {
+    Board board = new Board(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 0}});
+    MatcherAssert.assertThat(board.equals(board), IsEqual.equalTo(true));
+
+    Board boardTwo = new Board(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 0}});
+    MatcherAssert.assertThat(board.equals(boardTwo), IsEqual.equalTo(true));
+
+    Board boardThree = new Board(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 0, 8}});
+    MatcherAssert.assertThat(board.equals(boardThree), IsEqual.equalTo(false));
+
+    Board boardFour = new Board(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 0, 8}});
+    MatcherAssert.assertThat(boardThree.equals(boardFour), IsEqual.equalTo(true));
+  }
+
+  @Test
+  public void neighborsTest() {
+    Board board = new Board(new int[][]{{1, 2, 3},
+                                        {4, 0, 6},
+                                        {7, 8, 5}});
+    Iterator<Board> neighbors = board.neighbors().iterator();
+    MatcherAssert.assertThat(neighbors.next(), IsEqual.equalTo(new Board(new int[][]{{1, 0, 3},
+                                                                                     {4, 2, 6},
+                                                                                     {7, 8, 5}})));
+
+    MatcherAssert.assertThat(neighbors.next(), IsEqual.equalTo(new Board(new int[][]{{1, 2, 3},
+                                                                                     {4, 8, 6},
+                                                                                     {7, 0, 5}})));
+
+    MatcherAssert.assertThat(neighbors.next(), IsEqual.equalTo(new Board(new int[][]{{1, 2, 3},
+                                                                                     {0, 4, 6},
+                                                                                     {7, 8, 5}})));
+
+    MatcherAssert.assertThat(neighbors.next(), IsEqual.equalTo(new Board(new int[][]{{1, 2, 3},
+                                                                                     {4, 6, 0},
+                                                                                     {7, 8, 5}})));
+  }
+}
